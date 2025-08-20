4456b50729c88b060c8e52f510beff0796c98d65
rjrodriguezr <rjrodriguezr@gmail.com>
2025-08-20 11:55:26 -0500

    Company.js circular dependency solved

--- src/models/Company.js
+++ src/models/Company.js
@@ -1,7 +1,6 @@
 const logger = require('../../lib/logger');
 const { Schema, model } = require('mongoose');
 const { modelAuditPlugin } = require('../middlewares');
-const { companyService } = require('../services');
 
 const CompanySchema = Schema({
   name: {
@@ -120,6 +119,7 @@ CompanySchema.post('findOneAndUpdate', async function (doc) {
   // `this` es la Query.
   if (doc) { // Verificar que `doc` no sea null (si no se encontró el documento para actualizar)
     try {
+      const { companyService } = require('../services');
       await companyService.saveSettingInRedis(doc);
     } catch (error) {
       logger.error('Error al actualizar configuración en Redis tras "findOneAndUpdate":', error);
@@ -130,6 +130,7 @@ CompanySchema.post('findOneAndUpdate', async function (doc) {
 // Hook para guardar en Redis después de cada save o update
 CompanySchema.post('save', async function (doc) {
   try {
+    const { companyService } = require('../services');
     await companyService.saveSettingInRedis(doc);
   } catch (error) {
     logger.error('Error al guardar configuración en Redis:', error);
