a59b673bd5f4aad28903ef2ea775cb8a3a386580
rjrodriguezr <rjrodriguezr@gmail.com>
2025-09-13 20:56:50 -0500

    index.js se agrega modelo Campaign al index para que este expuesto a los demas modulos

--- src/models/index.js
+++ src/models/index.js
@@ -3,11 +3,13 @@ const Customer  = require('./Customer');
 const User  = require('./User');
 const Message  = require('./Message');
 const WapTemplate  = require('./WapTemplate');
+const Campaign  = require('./Campaign');
 
 module.exports = {
+    Campaign,
     Company,
     Customer,
-    User,
     Message,
+    User,
     WapTemplate,
 };
