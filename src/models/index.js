99baa729cac6efc4f490c565bd940995a1dad87e
rjrodriguezr <rjrodriguezr@gmail.com>
2025-09-16 00:01:05 -0500

    Refactor index.js to dynamically import models from the directory instead of hardcoding each model

--- src/models/index.js
+++ src/models/index.js
@@ -1,25 +1,26 @@
-const Campaign  = require('./Campaign');
-const Chat  = require('./Chat');
-const Company  = require('./Company');
-const Conversation  = require('./Conversation');
-const Customer  = require('./Customer');
-const Message  = require('./Message');
-const Product  = require('./Product');
-const ProductCategory  = require('./ProductCategory');
-const UnitsOfMeasure  = require('./UnitsOfMeasure');
-const User  = require('./User');
-const WapTemplate  = require('./WapTemplate');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = {
-    Campaign,
-    Chat,
-    Company,
-    Conversation,
-    Customer,
-    Message,
-    Product,
-    ProductCategory,
-    UnitsOfMeasure,
-    User,
-    WapTemplate,
-};
+const models = {};
+const basename = path.basename(__filename);
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    // Retornar solo archivos .js que no sean el index.js actual
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js'
+    );
+  })
+  .forEach(file => {
+    // Importar el modelo
+    const model = require(path.join(__dirname, file));
+    // Obtener el nombre del modelo (generalmente el nombre del archivo sin .js)
+    const modelName = path.basename(file, '.js');
+    // Asignarlo al objeto de modelos
+    models[modelName] = model;
+  });
+
+module.exports = models;
